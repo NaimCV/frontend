4f5a3dbfdf87c515199fe3df5d381040ec99a035
NaimCV <naimcv93@gmail.com>
2023-11-05 00:06:54 +0100

    home-page (#11)

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -1,11 +1,62 @@
 import React  from 'react'
 import SliderHome from '../componentes/home/slider_home'
 import '../index.css'
-import { Col, Row, Divider } from 'antd'
+import { Col, Row, Divider, Button } from 'antd'
 import { NavLink } from 'react-router-dom'
 import {BrowserView, MobileView} from 'react-device-detect'
+import GridDosImagenes from '../componentes/Templates/GridDosImagenes'
 
 export default function Home() {
+    const imagenesSesiones = [
+        {
+            titulo: "Maternidad",
+            imagenes: {
+                judith_55: 'https://oliveda-photography.es/wp-content/uploads/2023/01/JUDITH-55-2048x1365.jpg',
+                carolina_18: 'https://oliveda-photography.es/wp-content/uploads/2023/01/CAROLINA-18-2048x1365.jpg'
+            },
+            enlace: "/sesiones/maternidad"
+        },
+        {
+            titulo: "Newborn",
+            imagenes: {
+                bernat_09: 'https://oliveda-photography.es/wp-content/uploads/2023/01/BERNAT-09-edited-scaled.jpg',
+                dafne_2: 'https://oliveda-photography.es/wp-content/uploads/2023/01/DAFNE-20000-edited-scaled.jpg'
+            },
+            enlace: "/sesiones/newborn"
+        },
+        {
+            titulo: "Seguimiento",
+            imagenes: {
+                sofia_41: 'https://oliveda-photography.es/wp-content/uploads/2023/01/SOFIA-41-1536x1024.jpg',
+                sofia_59: 'https://oliveda-photography.es/wp-content/uploads/2023/01/SOFIA-59-1536x1024.jpg'
+            },
+            enlace: "/sesiones/seguimiento"
+        },
+        {
+            titulo: "Cake Smash",
+            imagenes: {
+                marc_02: 'https://oliveda-photography.es/wp-content/uploads/2023/01/MARC-02-4-edited-scaled.jpg',
+                marc_46: 'https://oliveda-photography.es/wp-content/uploads/2023/01/MARC-46-edited-scaled.jpg'
+            },
+            enlace: "/sesiones/cake-smash"
+        },
+        {
+            titulo: "Cake Smash Infantiles",
+            imagenes: {
+                marc_02: 'https://oliveda-photography.es/wp-content/uploads/2023/01/MARC-02-4-edited-scaled.jpg',
+                marc_46: 'https://oliveda-photography.es/wp-content/uploads/2023/01/MARC-46-edited-scaled.jpg'
+            },
+            enlace: "/sesiones/cake-smash-infantiles"
+        },
+        {
+            titulo: "Familiares",
+            imagenes: {
+                marc_02: 'https://oliveda-photography.es/wp-content/uploads/2023/01/MARC-02-4-edited-scaled.jpg',
+                marc_46: 'https://oliveda-photography.es/wp-content/uploads/2023/01/MARC-46-edited-scaled.jpg'
+            },
+            enlace: "/sesiones/familiar"
+        }
+    ]
     return (
         <>
             <body>
@@ -16,24 +67,14 @@ export default function Home() {
                     }}>
                     <SliderHome />
                 </div>
-                <Divider orientation='center'>
-                    <h2>Fotografía Maternidad, Newborn, Cake Smash y Familias.</h2>
-                </Divider>
                 <div>
-                <Divider orientation='center'><h2>Portfolio</h2></Divider>
                 <BrowserView>
-                    <div className='alinear-section-home'>
-                    <section>
-                        <img alt="portfolio embarazada" src='https://oliveda-photography.es/wp-content/uploads/2022/01/ESTEFANIA-home-scaled-e1641490737529.jpg'/>
-                        <img alt="portfolio newborn" src='https://oliveda-photography.es/wp-content/uploads/2022/02/THIAGO-31-scaled-e1643813248692.jpg'/>
-                        <img alt="portfolio seguimiento" src='https://oliveda-photography.es/wp-content/uploads/2022/01/LIA-10-scaled-e1643812923750.jpg'/>
-                        <img alt="portfolio cake-smash" src='https://oliveda-photography.es/wp-content/uploads/2022/01/SOFIA-home-scaled-e1641493347912.jpg'/>
-                        <img alt="portfolio bath-milk" src='https://oliveda-photography.es/wp-content/uploads/2022/01/VEGA-home-scaled-e1641493331319.jpg'/>
-                        <img alt="portfolio familiares" src='https://oliveda-photography.es/wp-content/uploads/2022/01/FAMILIAR-home-scaled-e1641493322614.jpg'/>
-                    </section>
-                    </div>
+                    {imagenesSesiones.map((sesion) => (
+                        <GridDosImagenes data={sesion}/>
+                    ))}
                 </BrowserView>
                 <MobileView>
+                    <Divider orientation='center'><h2>Portfolio</h2></Divider>
                     <Row gutter={[3,3]} justify='center'>
                         <Col span={6}>
                             <NavLink to='/sesiones/maternidad'>
