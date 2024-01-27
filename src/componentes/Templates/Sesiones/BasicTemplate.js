f0f762426b6e2be6bbf624d9d25b23243d87a4b1
NaimCV <naimcv93@gmail.com>
2024-01-27 20:28:50 +0100

    Cambios plantilla sesiones - card packs (#29)
    * wip
    
    * wip
    
    * cambios full

--- src/componentes/Templates/Sesiones/BasicTemplate.js
+++ src/componentes/Templates/Sesiones/BasicTemplate.js
@@ -1,21 +1,28 @@
 import { useMediaQuery } from 'react-responsive'
-import { Collapse, Image } from 'antd'
+import { Collapse, Image, Card, Col, Row, Carousel } from 'antd'
 import GridTresImagenes from '../GridTresImagenes'
 import DescripcionDetalladaSession from './DescripcionDetalladaSesion'
-import GridPrecios from '../GridPrecios'
-import GridQuatroImagenes from '../GridQuatroImagenes'
 import parse from 'html-react-parser'
+import { path_images } from '../../../consts'
+import GaleriaImagenes from '../Galeria_imagenes'
 
 import '../../../index.css'
+
+function numeroPacks (data) {
+    const cantidadPacks = data.length
+    if (cantidadPacks === 2) {
+        return 12
+    }
+    return 8
+}
 
 export default function BasicTemplate({data}) {
     const titulo = data.titulo
     const imagenPortada = data.imagenes.portada
-    const gridTriple = data.imagenes.grid_triple
+    const gridTriple1 = data.imagenes.grid_triple1
     const descripcionSesionDetallada = data.descripcioneDetallada
     const textoInicial = data.textos.inicial
     const dataPrecios = data.dataPrecios
-    const imagenesGridQuatro = data.imagenesGridQuatro
     const { Panel } = Collapse
     const dataCollapse = data.dataCollapse
     const descripcionSesionDesktop = data.descripcionSesionDesktop
@@ -34,19 +41,57 @@ export default function BasicTemplate({data}) {
             <Mobile>
             <h1 style={{ textAlign: 'center' }}>{titulo}</h1>
                 <div>
-                    <Image alt={Object.keys(imagenPortada)[0]} className='imagen-responsive-center-roudend' src={Object.values(imagenPortada)[0]}/>
+                    <Image alt={Object.keys(imagenPortada)[0]} className='imagen-responsive-center-roudend' src={path_images.replace('<image_id>', Object.values(imagenPortada)[0])}/>
                 </div>
                 <div className='description-align' style={{ marginTop: '10px' }}>
                     {textoInicial.map((texto) => (
                         <p>{texto}</p>
                     ))}
                 </div>
-                <GridTresImagenes gridTriple={gridTriple} />
+                <GridTresImagenes gridTriple={gridTriple1} />
                 {descripcionSesionDetallada ? <DescripcionDetalladaSession data={descripcionSesionDetallada} isMobile={true}/> : null}
-                <GridPrecios data={dataPrecios} isMobile={true} />
-                {Object.values(imagenesGridQuatro).map((val) => 
-                    <GridQuatroImagenes data={val}/>
-                )}
+                <div style={{marginTop: '50px'}}>
+                    {Object.values(dataPrecios).map((val) =>
+                        <Card
+                            bordered={true}
+                            style={{
+                                width: 'auto-fit',
+                                textAlign: 'center',
+                                marginTop: '50px',
+                                whiteSpace: 'pre-line'
+                            }}
+                            headStyle={{
+                                whiteSpace: 'pre-line'
+                            }}
+                        >
+                            {<h3>{val.titulo_opcion}</h3>}
+                            {val.descripcion.map((desc) =>
+                                <p>{desc}</p>
+                            )}
+                        </Card>
+                    )}
+                </div>
+                {data.plaquita ? 
+                    <div style={{ marginTop: '30px'}}>
+                        <h2 style={{ textAlign: 'center' }}>{data.plaquita.titulo}</h2>
+                        <p style={{ textAlign: 'center' }}>{data.plaquita.descripcion}</p>
+                        <Row
+                        gutter={{
+                            xs: 8,
+                            sm: 16,
+                            md: 24,
+                            lg: 32,
+                        }}
+                        >
+                           {data.plaquita.imagenes.map((imagen) => 
+                                <Col className='gutter-row' span={12}>
+                                    <Image className='imagen-responsive-center-roudend' style={{ width: '100%' }} src={path_images.replace('<image_id>', imagen)}/>
+                                </Col>
+                            )}
+                        </Row>
+                    </div>
+                : null
+                }
                 <div style={{
                 marginTop: '30px',
                 textAlign: 'center',
@@ -61,29 +106,120 @@ export default function BasicTemplate({data}) {
                         ))}
                     </Collapse>
                 </div>
+                {data.carousel ?
+                <div style={{marginTop: '50px'}}>
+                    <Carousel autoplay dots={false} fade={true}>
+                        <div>
+                            <Image className='imagen-responsive-center-roudend' alt='marron' src={path_images.replace('<image_id>', 'f1d749b9-d3bc-466b-088c-bf122f481300')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo marron</div>
+                        </div>
+                        <div>
+                            <Image className='imagen-responsive-center-roudend' alt='gris' src={path_images.replace('<image_id>', 'dcdc6919-a006-44a8-a7b3-464a6d7f3900')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo gris</div>
+                        </div>
+                        <div>
+                            <Image className='imagen-responsive-center-roudend' alt='negro' src={path_images.replace('<image_id>', '75e41a73-71cd-403f-64e4-addb1c43e900')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo negro</div>
+                        </div>
+                        <div>
+                            <Image className='imagen-responsive-center-roudend' alt='blanco' src={path_images.replace('<image_id>', 'dab818ae-299e-4702-c2c0-051ac374b400')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo blanco</div>
+                        </div>
+                    </Carousel>
+                </div>
+                : null}
+                { data.galeria_imagenes ?
+                <div>
+                    <h2 style={{ textAlign: 'center', marginTop: '50px' }}>Galería {titulo}</h2>
+                    <GaleriaImagenes data={data.galeria_imagenes}/>
+                </div>
+                : null}
             </Mobile>
             <Default>
                 <h1 style={{ textAlign: 'center' }}>{titulo}</h1>
                 <div className='center'>
-                    <Image alt={Object.keys(imagenPortada)[0]} className='imagen-responsive-center-roudend' src={Object.values(imagenPortada)[0]}/>
+                    <Image alt={Object.keys(imagenPortada)[0]} className='imagen-responsive-center-roudend' src={path_images.replace('<image_id>', Object.values(imagenPortada)[0])}/>
                 </div>
                 <div className='description-align'>
                 {textoInicial.map((texto) => (
                         <p>{texto}</p>
                     ))}
                 </div>
-                <GridTresImagenes gridTriple={gridTriple}/>
+                <GridTresImagenes gridTriple={gridTriple1}/>
                 {descripcionSesionDetallada ? <DescripcionDetalladaSession data={descripcionSesionDetallada} isMobile={false}/> : null}
-                <GridPrecios data={dataPrecios} isMobile={false} />
-                {Object.values(imagenesGridQuatro).map((val) => 
-                    <GridQuatroImagenes data={val}/>
-                )}
+                <div style={{ marginTop: '50px'}}>
+                    <Row gutter={16}>
+                        {Object.values(dataPrecios).map((val) => 
+                            <Col span={numeroPacks(dataPrecios)}>
+                                <Card
+                                    title={val.titulo_opcion}
+                                    bordered={false}
+                                    style={{textAlign: 'center'}}
+                                >
+                                    {val.descripcion.map((desc) => 
+                                        <p>{desc}</p>
+                                    )}
+                                </Card>
+                            </Col>
+                        )}
+                    </Row>
+                </div>
+                {data.plaquita ?
+                    <div style={{ marginTop: '50px'}}>
+                        <h2 style={{ textAlign: 'center' }}>{data.plaquita.titulo}</h2>
+                        <p style={{ textAlign: 'center' }}>{data.plaquita.descripcion}</p>
+                        <Row
+                        gutter={{
+                            xs: 8,
+                            sm: 16,
+                            md: 24,
+                            lg: 32,
+                        }}
+                        style={{ marginTop: '20px'}}
+                        >
+                            {data.plaquita.imagenes.map((imagen) => 
+                                <Col className='gutter-row' span={12}>
+                                    <Image className='imagen-responsive-center-roudend' style={{ width: '100%' }} src={path_images.replace('<image_id>', imagen)}/>
+                                </Col>
+                            )}
+                        </Row>
+                    </div>
+                : null
+                }
                 <div style={{
                     marginTop: '50px',
                     textAlign: 'center'
                 }}>
                     {parse(descripcionSesionDesktop)}          
                 </div>
+                {data.carousel ?
+                <div style={{ marginTop: '50px', textAlign: 'center' }}>
+                    <Carousel autoplay dots={false} fade={true}>
+                        <div className='alinear-carousel'>
+                            <Image className='imagen-responsive-center-roudend' alt='marron' src={path_images.replace('<image_id>', 'f1d749b9-d3bc-466b-088c-bf122f481300')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo marron</div>
+                        </div>
+                        <div className='alinear-carousel'>
+                            <Image className='imagen-responsive-center-roudend' alt='gris' src={path_images.replace('<image_id>', 'dcdc6919-a006-44a8-a7b3-464a6d7f3900')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo gris</div>
+                        </div>
+                        <div className='alinear-carousel'>
+                            <Image className='imagen-responsive-center-roudend' alt='negro' src={path_images.replace('<image_id>', '75e41a73-71cd-403f-64e4-addb1c43e900')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo negro</div>
+                        </div>
+                        <div className='alinear-carousel'>
+                            <Image className='imagen-responsive-center-roudend' alt='blanco' src={path_images.replace('<image_id>', 'dab818ae-299e-4702-c2c0-051ac374b400')}/>
+                            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '18px' }}>Fondo blanco</div>
+                        </div>
+                    </Carousel>
+                </div>
+                : null}
+                { data.galeria_imagenes ?
+                <div>
+                    <h2 style={{ textAlign: 'center', marginTop: '50px' }}>Galería {titulo}</h2>
+                    <GaleriaImagenes data={data.galeria_imagenes}/>
+                </div>
+                : null}
             </Default>
         </div>
     )
--- src/pages/sesiones/Newborn.js
+++ src/pages/sesiones/Newborn.js
@@ -1,16 +1,48 @@
 import BasicTemplate from "../../componentes/Templates/Sesiones/BasicTemplate"
-import { Image } from 'antd'
-import { path_images } from "../../consts"
 
 export default function Newborn() {
     const imagenes = {
-        portada: {newborn1: path_images.replace('<image_id>', '40386b12-a5d9-4e22-62e4-645b255b2800')},
-        grid_triple: {
-            newborn2: path_images.replace('<image_id>', 'adbc4ccf-80ad-463b-1eef-60331667d700'),
-            newborn3: path_images.replace('<image_id>', '242b1cd6-6538-4a54-943c-9067d537bb00'),
-            newborn4: path_images.replace('<image_id>', '7e74f4c9-f8ea-4083-22ac-cd3780868200')
+        portada: {newborn1: '40386b12-a5d9-4e22-62e4-645b255b2800'},
+        grid_triple1: {
+            newborn2: 'adbc4ccf-80ad-463b-1eef-60331667d700',
+            newborn3: '242b1cd6-6538-4a54-943c-9067d537bb00',
+            newborn4: '7e74f4c9-f8ea-4083-22ac-cd3780868200'
+        },
+        grid_triple2: {
+            newborn5: 'e30d08e2-c37c-4510-a129-a9bc390e5400',
+            newborn6: '37895e5e-989b-48d5-1525-450b41a39e00',
+            newborn7: '78a0c77c-3327-462b-b4ac-bfe56ba2dc00'
+        },
+        grid_triple3: {
+            newborn9: '40146f86-b823-4d8c-4324-cf2121a89500',
+            newborn10: '7fcb118c-72a8-42f7-00ce-01b95afd8500',
+            newborn11: '3f2c75b1-c1a3-4055-0d18-48c56c52c700'
+        },
+        grid_triple4: {
+            newborn9: 'b206e5a4-3c2d-4a82-58b7-1cdb1eccdc00',
+            newborn10: '7f22f5c6-a2a9-44e2-dd6e-07bfd0067300',
+            newborn11: '2b7b1dad-63c6-4427-5da6-f42ceec0a800'
+        },
+        grid_triple5: {
+            newborn9: 'd8674c39-d0f8-417d-d590-a14905a43400',
+            newborn10: '604d8959-6fb7-44e8-c10e-508f38f82600',
+            newborn11: '01c7d37e-9feb-41d0-284c-9e69102eda00'
         }
     }
+    const galeria_imagenes = [
+        'e30d08e2-c37c-4510-a129-a9bc390e5400',
+        '37895e5e-989b-48d5-1525-450b41a39e00',
+        '78a0c77c-3327-462b-b4ac-bfe56ba2dc00',
+        '40146f86-b823-4d8c-4324-cf2121a89500',
+        '7fcb118c-72a8-42f7-00ce-01b95afd8500',
+        '3f2c75b1-c1a3-4055-0d18-48c56c52c700',
+        'b206e5a4-3c2d-4a82-58b7-1cdb1eccdc00',
+        '7f22f5c6-a2a9-44e2-dd6e-07bfd0067300',
+        '2b7b1dad-63c6-4427-5da6-f42ceec0a800',
+        'd8674c39-d0f8-417d-d590-a14905a43400',
+        '604d8959-6fb7-44e8-c10e-508f38f82600',
+        '01c7d37e-9feb-41d0-284c-9e69102eda00'
+    ]
     const textos = {
         inicial: [
             'La dulce espera ha llegado, tienes en tus brazos a esa personita que as estado gestando tantos meses y al fin puedes verla, tocarla y besarla.',
@@ -45,28 +77,6 @@ export default function Newborn() {
             ]
         }
     ]
-    const imagenesGridQuatro = {
-        primer_grid: {
-            imagen_grande: {
-                newborn1: path_images.replace('<image_id>', '0212c76d-def0-4e5a-9aa8-dd677da01a00')
-            },
-            imagenes_triple: {
-                newborn5: path_images.replace('<image_id>', 'f0df6a81-3575-415e-e568-de00c8f9e900'),
-                newborn6: path_images.replace('<image_id>', '377410d8-60b3-4b48-d894-637f57357300'),
-                newborn7: path_images.replace('<image_id>', '619b88df-251b-4d34-6a68-bf71975e3c00')
-            }
-        },
-        segundo_grid: {
-            imagen_grande: {
-                newborn1: path_images.replace('<image_id>', '0212c76d-def0-4e5a-9aa8-dd677da01a00')
-            },
-            imagenes_triple: {
-                newborn9: path_images.replace('<image_id>', '6f69e465-7e06-4783-d8bb-e7a0feba3100'),
-                newborn10: path_images.replace('<image_id>', 'ba68fbdf-ec51-40fc-eb2f-4e1de7189300'),
-                newborn11: path_images.replace('<image_id>', '1482efe1-ae98-42a5-897c-b915341ace00')
-            }
-        }
-    }
     const dataCollapse = [
         {'¿Cuándo hay que reservar esta sesión?': 'Lo ideal es reservar esta sesión en la semana 25/30 de gestación. Esta sesión se reserva con la fecha prevista de parto.'},
         {'¿Cuándo se realiza?': 'Esta sesión se realiza entre los 6 y 15 primeros días de vida, ya que el recién nacido mantiene la posición fetal y el sueño profundo. Una vez pasados estos días también se puede realizar la sesión, pero hay muchas poses que no se podrán realizar por la seguridad del bebe.'},
@@ -77,15 +87,21 @@ export default function Newborn() {
         {'¿Forma de pago?': 'Hay que hacer una paga y señal de 50 € para la reserva de la sesión, la misma se devolverá en caso de no poder asistir por motivos justificables o avisando 24 h antes de la sesión. El resto se pagará en EFECTIVO el mismo día de la sesión. La paga y señal hay que realizarla una vez os hayáis puesto en contacto conmigo y hayamos reservado día y hora. Datos para el Bizum paga y señal: 640019860 ¡MUY IMPORTANTE! Concepto nombre del recién nacido + newborn EJEMPLO: Martina newborn'}
     ]
     const descripcionSesionDesktop = "<h2>¿Cuándo hay que reservar esta sesión?</h2><p>Lo ideal es reservar esta sesión en la semana 25/30 de gestación. Esta sesión se reserva con la fecha prevista de parto.</p><h2>¿Cuándo se realiza?</h2><p>Esta sesión se realiza entre los 6 y 15 primeros días de vida, ya que el recién nacido mantiene la posición fetal y el sueño profundo. Una vez pasados estos días también se puede realizar la sesión, pero hay muchas poses que no se podrán realizar por la seguridad del bebe.</p><h2>¿Quién puede participar en la sesión?</h2><p>El protagonista es el recién nacido, pero l@s hermanit@s y los papis, mamis  también pueden participar. ¡MASCOTAS! Si pueden participar las mascotas. Siempre avisando con tiempo y teniendo en cuenta que si la mascota no está acostumbrada a ir a sitios nuevos, tendrá que haber una persona responsable para gestionar sus necesidades sin interrumpir la sesión.</p><h2>¿Qué tengo que traer?</h2><p>'Dispongo de ropita y accesorios de todos los colores y tallas. No os tenéis que preocupar de nada. No obstante, si tenéis algún conjunto o algún detalle que queráis fotografiar, será bienvenido. Siempre avisando con tiempo para cuadrar el escenario.</p><h2>El recién nacido tiene granitos, manchas, arañazos…</h2><p>Los recién nacidos nacen con todas estas cositas, es normal que los primeros días de vida salgan granitos, se arañen o tengan rojeces. Mi edición final elimina todo.</p><h2>¿Quieres una plaquita con su nombre?</h2><p>Te damos la opción de personalizar aún más esta sesión. Precio del nombre <b>10€</b> en madera <b>15€</b> con toda la información del nacimiento del bebe.</p><h2>¿Forma de pago?</h2><p>Hay que hacer una paga y señal de 50 € para la reserva de la sesión, la misma se devolverá en caso de no poder asistir por motivos justificables o avisando 24 h antes de la sesión.</p><p>El resto se pagará en <b>EFECTIVO</b> el mismo día de la sesión. La paga y señal hay que realizarla una vez os hayáis puesto en contacto conmigo y hayamos reservado día y hora. Datos para el Bizum paga y señal: 640019860 <b>¡MUY IMPORTANTE!</b> Concepto nombre del recién nacido + newborn EJEMPLO: Martina newborn</p>"
+    const plaquita = {
+        titulo: "¿Quieres una plaquita personalizada con su nombre?",
+        descripcion: "Te damos la opción de personalizar aún más esta sesión. Precio de la plaquita 10 €.",
+        imagenes: ["46007b9a-2802-4646-b1b9-ea44e572f900", "f5531c02-1466-44cf-534a-a71ac3320100"]
+    }
     const data = {
         titulo: 'Newborn',
         imagenes,
         textos,
         descripcioneDetallada,
         dataPrecios,
-        imagenesGridQuatro,
         dataCollapse,
-        descripcionSesionDesktop
+        descripcionSesionDesktop,
+        plaquita,
+        galeria_imagenes
     }
     return (
     <>
@@ -94,7 +110,6 @@ export default function Newborn() {
         marginTop: '50px',
         textAlign: 'center'
     }}>
-        {/* <Image movable={false} style={{ borderRadius: '9999px' }} alt='newborn12' src='https://oliveda-photography.es/wp-content/uploads/2023/01/ELAN-12-edited-scaled.jpg' /> */}
     </div>
     </>
     )
